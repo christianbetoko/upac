e56ae97eaf553b9cfc77df914ad22d7056efe043
betoko <christianbetoko@gmail.com>
2026-09-07 07:29:59 +0100

    route evaluation jour 1a

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -147,6 +147,21 @@
                             </div>
                         </div>
                     </div>
+                      <div class="col-xl-12">
+                        <div class="rs-feature-wrapper ">
+                            <div class="rs-feature-item">
+                                
+                            </div>
+                            <div class="rs-feature-item">
+                                <a href="{{ route('evaluation-jour1') }}" >
+                                    Evaluation Jour 1
+                                </a>
+                            </div>
+                           <div class="rs-feature-item">
+                               
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
