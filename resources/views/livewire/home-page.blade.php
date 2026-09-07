0b49dd800e4cd9a953f2af0030b203c2a00319e6
betoko <christianbetoko@gmail.com>
2026-09-07 07:42:58 +0100

    route evaluation jour 1f

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -132,7 +132,7 @@
         <div class="rs-feature-area rs-feature-two">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-xl-10">
+                    <div class="col-xl-12">
                         <div class="rs-feature-wrapper ">
                             <div class="rs-feature-item">
                                 
@@ -147,7 +147,11 @@
                             </div>
                         </div>
                     </div>
-                      <div class="col-xl-10">
+                   
+                </div>
+                <div class="row">
+                    
+                      <div class="col-xl-12">
                         <div class="rs-feature-wrapper ">
                             <div class="rs-feature-item">
                                 
